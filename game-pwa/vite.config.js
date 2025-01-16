d90cf58fbee19ecddc5efda1bef5719c006e1157
JavidelaRubia <javidelarubia@gmail.com>
2025-01-16 09:37:34 +0100

    Delete-css

---
+++ game-pwa/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Vite PWA App',
+        short_name: 'VitePWA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#0078d4',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    port: 3000, // Puedes cambiar el puerto si lo deseas
+  },
+});
